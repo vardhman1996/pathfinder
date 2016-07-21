4592a6332e357deae79c8e1022d2ca1eef5fe290
Vardhman Mehta <mehtav@cs.washington.edu>
2016-07-20 20:49:03 -0700

    basic django setup

--- pathui/pathui/settings.py
+++ pathui/pathui/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'pathui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'pathui', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
